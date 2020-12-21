f4faeb494530936039a341cc3580e07e063db18b
Joseph Mayrose <joseph.mayrose@gmail.com>
2020-12-21 00:02:08 -0500

    WIP: playing a hand of blackjack

--- fish.go
+++ fish.go
@@ -3,8 +3,10 @@ package main
 //TODO Eventually move Card to a golang module
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
 )
 
@@ -12,8 +14,6 @@ import (
 type Card struct {
 	value int
 	suit  string
-	//TODO Maybe change string to char?
-	//? does char exist in golang?
 }
 
 func printCard(card Card) {
@@ -35,7 +35,7 @@ func printHand(hand []Card) {
 	for eachCard := range hand {
 		printCard(hand[eachCard])
 	}
-	println("")
+	fmt.Println("")
 }
 
 func makeDeck() []Card {
@@ -71,7 +71,38 @@ func drawCards(hand []Card, deck *[]Card, numCards int) []Card {
 	return hand
 }
 
-func playBlackjack() int {
+func playBlackhackHand(hand *[]Card, deck *[]Card) {
+	var canSplit bool = (*hand)[0].suit == (*hand)[1].suit
+
+	//TODO Keep prompting until bust or stand
+	for {
+		fmt.Println("[1] Hit")   //hit
+		fmt.Println("[2] Stand") //stand
+		if canSplit {
+			fmt.Println("[3] Split") //split
+		}
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+
+			switch scanner.Text() {
+			case "1":
+				//Hit
+				fmt.Println("Hit")
+			case "2":
+				//Stand
+				fmt.Println("Stand")
+			case "3":
+				if canSplit {
+					//Split
+					fmt.Println("Split")
+					//TODO Play new hands recursively
+				}
+			}
+		}
+	}
+}
+
+func playBlackjackGame() int {
 	var deck = makeDeck()
 	var playerHand []Card
 	var dealerHand []Card
@@ -79,38 +110,43 @@ func playBlackjack() int {
 	playerHand = drawCards(playerHand, &deck, 2)
 	dealerHand = drawCards(dealerHand, &deck, 2)
 
-	//TODO Prompt for hit, stand, double down, or split
+	playBlackhackHand(&playerHand, &deck)
 
-	if getBlackjackHandValue(playerHand) < 21 {
-		for getBlackjackHandValue(dealerHand) < 17 {
-			dealerHand = drawCards(dealerHand, &deck, 1)
-		}
+	if getHandValue(playerHand) > 21 {
+		fmt.Println("Player Busts, Dealer Wins")
+		return -1
+	}
+	for getHandValue(dealerHand) < 17 {
+		dealerHand = drawCards(dealerHand, &deck, 1)
 	}
 
-	printHand(playerHand)
-	printHand(dealerHand)
 	return resolveBlackjack(playerHand, dealerHand)
 }
 
 func resolveBlackjack(player []Card, dealer []Card) int {
-	var playerScore = getBlackjackHandValue(player)
-	var dealerScore = getBlackjackHandValue(dealer)
+	var playerScore = getHandValue(player)
+	var dealerScore = getHandValue(dealer)
 
-	println(playerScore, " ", dealerScore)
+	printHand(player)
+	printHand(dealer)
+	fmt.Println(playerScore, " ", dealerScore)
 
-	if playerScore > dealerScore {
-		println("Player wins")
+	if dealerScore > 21 {
+		fmt.Println("Dealer Busts, Player wins")
+		return 1
+	} else if playerScore > dealerScore {
+		fmt.Println("Player Beats Dealer, Player wins")
 		return 1
 	} else if playerScore == dealerScore {
-		println("Push")
+		fmt.Println("Push")
 		return 0
 	} else {
-		println("Dealer Wins")
+		fmt.Println("Dealer Wins")
 		return -1
 	}
 }
 
-func getBlackjackHandValue(hand []Card) int {
+func getHandValue(hand []Card) int {
 	var points int = 0
 	var workingCardVal int
 	for x := 0; x < len(hand); x++ {
@@ -119,7 +155,7 @@ func getBlackjackHandValue(hand []Card) int {
 			if workingCardVal == 1 {
 				points += 11
 				//TODO Contend with 1/11 split of Aces
-				//?Accomplish this by returning array of ints with both possible points
+				//?Accomplish this by returning array of ints with both possible points??
 				// points++
 			} else {
 				points += workingCardVal
@@ -133,5 +169,5 @@ func getBlackjackHandValue(hand []Card) int {
 
 func main() {
 	//TODO Menu to select between different games
-	playBlackjack()
+	playBlackjackGame()
 }
